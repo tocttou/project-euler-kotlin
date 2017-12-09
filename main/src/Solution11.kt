06651172ca35ae0bee56942860b3cd57fbf8458b
Ashish Chaudhary <me@ashishchaudhary.in>
2017-12-09 12:31:36 +0530

    added problem 11

---
+++ main/src/Solution11.kt
@@ -0,0 +1,90 @@
+package src
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val d = 20
+    val inputStream = File("main/src/data/Problem11Data.txt").inputStream()
+    val content = inputStream.bufferedReader().use { it.readText() }
+
+    val horizontalSplit: List<List<Int>> = content.split("\n")
+        .toMutableList()
+        .map { it.split(" ") }
+        .map { it.toMutableList().map { v -> v.toInt() } }
+    val verticalSplit = mutableListOf<MutableList<Int>>()
+
+    horizontalSplit.mapIndexed { _, list ->
+        for (i in 0..(d - 1)) {
+            verticalSplit.add(mutableListOf<Int>())
+            verticalSplit[i].add(list[i])
+        }
+    }
+    val rightDiagonalSplit = replaceIndexPairsWithValue(verticalSplit, generateIndexList(d))
+    val leftDiagonalSplit = replaceIndexPairsWithValue(
+        reversedGridByRow(horizontalSplit),
+        generateIndexList(d)
+    )
+    val maxProductOfWindowLengthFour = mutableListOf<Int?>(
+        maxOfWindowProducts(horizontalSplit, 4),
+        maxOfWindowProducts(verticalSplit, 4),
+        maxOfWindowProducts(leftDiagonalSplit, 4),
+        maxOfWindowProducts(rightDiagonalSplit, 4)
+    ).filterNotNull().max()
+    println(maxProductOfWindowLengthFour)
+}
+
+fun maxOfWindowProducts(splits: List<List<Int>>, windowLength: Int): Int? {
+    return splits.toMutableList().map {
+        it.windowed(windowLength).map { v ->
+            v.reduce { acc, i -> acc * i }
+        }
+    }.flatten().max()
+}
+
+fun reversedGridByRow(grid: List<List<Int>>): List<List<Int>> {
+    val list = mutableListOf<List<Int>>()
+    grid.map { list.add(it.reversed()) }
+    return list.toList()
+}
+
+fun replaceIndexPairsWithValue(
+    grid: List<List<Int>>,
+    indexList: List<MutableList<Pair<Int, Int>>>
+): MutableList<MutableList<Int>> {
+    val list = mutableListOf<MutableList<Int>>()
+    indexList.map {
+        val temp = mutableListOf<Int>()
+        for (i in it) {
+            temp.add(grid[i.first][i.second])
+        }
+        list.add(temp)
+    }
+    return list
+}
+
+fun generateIndexList(size: Int): List<MutableList<Pair<Int, Int>>> {
+    val lValue = (0..(size - 1)).map {
+        generateListOfPairs(0, it)
+    }.toMutableList()
+    val rValue = (size..2 * (size - 1)).mapIndexed { index, i ->
+        generateListOfPairs(index + 1, i, "right")
+    }
+    return lValue + rValue
+}
+
+fun generateListOfPairs(_anchor: Int, _sum: Int, direction: String = "left")
+    : MutableList<Pair<Int, Int>> {
+    val list = mutableListOf<Pair<Int, Int>>()
+    var anchor = _anchor
+    var sum = _sum
+    while (if (direction == "left") sum >= _anchor else _sum - anchor >= _anchor) {
+        if (direction == "left") {
+            list.add(Pair(anchor, sum))
+        } else {
+            list.add(Pair(anchor, _sum - anchor))
+        }
+        anchor++
+        sum--
+    }
+    return list
+}
